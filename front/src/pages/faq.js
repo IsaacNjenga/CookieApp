b69d3009fb5339e5651df3ace69d29b3871002d3
Isaac Njenga <ayzzoh20@gmail.com>
2025-02-27 12:26:23 +0300

    ifdha

--- front/src/pages/faq.js
+++ front/src/pages/faq.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Collapse, Space, theme, Typography } from "antd";
 import { faq } from "../assets/data/data";
 import { CaretRightOutlined } from "@ant-design/icons";
@@ -7,9 +7,9 @@ const { Title } = Typography;
 
 function Faq() {
   const { token } = theme.useToken();
-
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
   return (
-    <div style={{ padding: "0px", maxWidth: "800px", margin: "0 auto" }}>
+    <div style={{ maxWidth: "800px", margin: "0 auto" }}>
       <Title level={2} style={{ textAlign: "center", marginBottom: "20px" }}>
         Frequently Asked Questions
       </Title>
@@ -19,16 +19,25 @@ function Faq() {
           collapsible="header"
           items={faq.map((f) => ({
             key: f.id,
-            label: <h4 style={{ margin: 0 }}>{f.question}</h4>,
+            label: (
+              <h2
+                style={{
+                  margin: 0,
+                  fontSize: isMobile ? "0.58rem" : "1.5rem",
+                }}
+              >
+                {f.question}
+              </h2>
+            ),
             children: <p style={{ margin: 0 }}>{f.answer}</p>,
           }))}
           defaultActiveKey={["1"]}
           bordered={true}
-          size="large"
+          size={isMobile ? "small" : "large"}
           expandIcon={({ isActive }) => (
             <CaretRightOutlined rotate={isActive ? 90 : 0} />
           )}
-          style={{ background: token.colorBgContainer }}
+          style={{ background: token.colorBgContainer, width: "100%" }}
         />
       </Space>
     </div>
